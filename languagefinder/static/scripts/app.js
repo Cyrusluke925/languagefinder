8c81b732f9421be0e555af60a649d541dd9d53b1
Luke <luke@Lukes-MacBook-Pro.local>
2018-12-09 13:23:16 -0800

    created ajax call for new markers

--- languagefinder/static/scripts/app.js
+++ languagefinder/static/scripts/app.js
@@ -51,10 +51,13 @@
         zoom: 10
         });
 
+        let color = 'blue'
+
     let marker = new google.maps.Marker({
         position: latlng,
         map: map,
-        title: "Marker Example"
+        title: "Marker Example",
+        icon: 'http://maps.google.com/mapfiles/ms/icons/'+color+'.png'
     })
     document.forms['addressForm'].reset()
     }
@@ -63,3 +66,75 @@
 
 
 
+let color;
+
+$('#JavaScript').on('click', function javaScriptText() {
+    $('#menuLink1').text('JavaScript')
+    color = 'yellow'
+})
+
+$('#Python').on('click', function javaScriptText() {
+    $('#menuLink1').text('Python')
+    color = 'green'
+})
+
+$('#Ruby').on('click', function javaScriptText() {
+    $('#menuLink1').text('Ruby')
+    color = 'red'
+})
+
+
+$('.geoCodingFormSearch').on('submit', function(e) {
+    e.preventDefault();
+    let language = $('#menuLink1').text()
+    let inputFieldData = $('.addressBar').val();
+    let newString = geoString(inputFieldData)
+    var geoLocationUrl = `${geoUrl}address=${newString}&key=${apiKey}`
+
+    
+
+
+    
+    if (language !== 'Languages') {
+        $.ajax({
+            method: "GET",
+            url: `${geoLocationUrl}`,
+            success: newAddressSuccess,
+            error: function addressError(err1, err2, err3) {
+                console.log(err1)
+                console.log(err2)
+                console.log(err3)
+            }
+    })
+}
+
+function newAddressSuccess(response) {
+    console.log(color)
+    let latitudeLocation = response.results[0].geometry.location.lat;
+    let longitudeLocation = response.results[0].geometry.location.lng;
+    let markerData = {
+        title: language,
+        latitude: latitudeLocation,
+        longitude: longitudeLocation,
+        marker: 'http://maps.google.com/mapfiles/ms/icons/'+color+'.png'
+    }
+
+    console.log(`The Latitude is ${latitudeLocation} and the longitude is ${longitudeLocation}` )
+    let latlng = {lat: latitudeLocation, lng: longitudeLocation}
+
+    $.ajax({
+        method: 'POST',
+        url: `language/new`,
+        data: markerData,
+        success: function onSuccess(response) {
+            console.log('success!!')
+        },
+        error: function onError(err1, err2, err3) {
+            console.log(err1)
+        }
+
+    })
+
+}
+
+})
